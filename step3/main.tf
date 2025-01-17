8f627230c5d00c23f8e37feb7ba26cec97b822e7
Volodymyr Kovalchuk <102913549+vkovalchuk-91@users.noreply.github.com>
2025-01-18 00:58:23 +0200

    Create main.tf

---
+++ step3/main.tf
@@ -0,0 +1,168 @@
+terraform {
+  backend "s3"{
+  bucket = "github-actions-slengpack"
+  key = "terrraform.tfstate"
+  region = "eu-central-1"
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# --- ECS Cluster ---
+resource "aws_ecs_cluster" "wordpress_cluster" {
+  name = "wordpress-cluster"
+}
+
+# --- IAM Policy Attachments ---
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_secrets_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ec2_container_service_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ssm_full_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_cloudwatch_full_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+# --- Security Group ---
+resource "aws_security_group" "ecs_sg" {
+  name_prefix = "ecs-wordpress-"
+  vpc_id      = "vpc-065d978e7afd01ff4" # use your VPC
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- ALB ---
+resource "aws_lb" "wordpress_alb" {
+  name               = "wordpress-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_sg.id]
+  subnets            = ["subnet-0094cfbc1db6f9890", "subnet-0aaed2193f0b00b29"] #use public subnet
+}
+
+# --- Target Group ---
+resource "aws_lb_target_group" "wordpress_tg" {
+  name        = "wordpress-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "vpc-065d978e7afd01ff4" # use your VPC
+  target_type = "ip"
+}
+
+# --- Listener ---
+resource "aws_lb_listener" "wordpress_listener" {
+  load_balancer_arn = aws_lb.wordpress_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress_tg.arn
+  }
+}
+
+# --- Task Definition ---
+resource "aws_ecs_task_definition" "wordpress" {
+  family                   = "wordpress"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "wordpress"
+      image     = "974101771016.dkr.ecr.eu-central-1.amazonaws.com/wordpress-repo:custom" # use your docker image form ECR
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "WORDPRESS_DB_HOST"
+          value = "kowo-db-instance.cf2ociwtdw3j.eu-central-1.rds.amazonaws.com" # use your endpoint
+        }
+      ]
+    }
+  ])
+}
+
+# --- ECS Service ---
+resource "aws_ecs_service" "wordpress" {
+  name            = "wordpress-service"
+  cluster         = aws_ecs_cluster.wordpress_cluster.id
+  task_definition = aws_ecs_task_definition.wordpress.arn
+  desired_count   = 1
+
+  launch_type = "FARGATE"
+
+  enable_execute_command = true
+
+  network_configuration {
+    subnets         = ["subnet-008e9be294a87fbaa", "subnet-0dff3902ba44b56a5"] #use private subnet
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.wordpress_tg.arn
+    container_name   = "wordpress"
+    container_port   = 80
+  }
+}
